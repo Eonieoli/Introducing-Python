ead42898244becf1fc6504c8c2afbd0276f718b7
Jaehyeok Kim <rublin322@gmail.com>
2026-01-21 11:11:35 +0900

    Chapter 3

---
+++ Chapter 3/int.py
@@ -0,0 +1,136 @@
+print(5)
+print(0)
+# print(05)
+
+print(123)
+print(+123)
+print(-123)
+
+a = 1,000,000
+print(a)
+
+million = 1_000_000
+print(million)
+
+print(1_2_3)
+
+
+print(5 + 9)
+print(100 - 7)
+print(4 - 10)
+
+print(5 + 9 + 3)
+print(4 + 3 - 2 - 1 + 6)
+print(5+9    +        3)
+
+print(6 * 7)
+print(7 * 6)
+print(6 * 7 * 2 * 3)
+
+print(9 / 5)
+print(9 // 5)
+
+# print(5 / 0)
+# print(7 // 0)
+
+a = 95
+print(a)
+print(a - 3)
+
+print(a)
+a = a - 3
+print(a)
+
+a = 95
+temp = a - 3
+a = temp
+print(a)
+
+a = 95
+a -= 3
+print(a)
+
+a = 92
+a += 8
+print(a)
+
+a = 100
+a *= 2
+print(a)
+
+a = 200
+a /= 3
+print(a)
+
+a = 13
+a //= 4
+print(a)
+
+print(9 % 5)
+
+print(divmod(9, 5))
+
+print(9 // 5)
+print(9 % 5)
+
+print(2 ** 3)
+print(2.0 ** 3)
+print(2 ** 3.0)
+print(0 ** 3)
+
+
+print(2 + 3 * 4)
+print(2 + (3 * 4))
+
+print(-5 ** 2)
+print(-(5 ** 2))
+print((-5) ** 2)
+
+
+print(10)
+print(0b10)
+print(0o10)
+print(0x10)
+
+value = 65
+print(bin(value))
+print(oct(value))
+print(hex(value))
+
+print(chr(65))
+print(ord('A'))
+
+
+print(int(True))
+print(int(False))
+
+print(bool(1))
+print(bool(0))
+
+print(int(98.6))
+print(int(1.0e4))
+
+print(int('99'))
+print(int('-23'))
+print(int('+12'))
+print(int('1_000_000'))
+
+print(int('10', 2))    # 2진수(binary)
+print(int('10', 8))    # 8진수(octal)
+print(int('10', 16))   # 16진수(hexadecimal)
+print(int('10', 22))   # 22진수
+
+
+print(12345)
+# print(int('99 bottles of beer on the wall'))
+# print(int('98.6'))
+# print(int('1.0e4'))
+print(4 + 7.0)
+
+print(True + 2)
+print(False + 5.0)
+
+
+googol = 10 ** 100
+print(googol)
+print(googol * googol)
---
+++ Chapter 3/problem.py
@@ -0,0 +1,19 @@
+# 3.1
+print(1 * 60 * 60)
+
+# 3.2
+seconds_per_hour = 1 * 60 * 60
+print(seconds_per_hour)
+
+# 3.3
+print(seconds_per_hour * 24)
+
+# 3.4
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day)
+
+# 3.5
+print(seconds_per_day / seconds_per_hour)
+
+# 3.6
+print(seconds_per_day // seconds_per_hour)
