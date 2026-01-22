8eea480eda24a4eaf01ee8a18203eed3937d80c6
김재혁 <rublin322@gmail.com>
2026-01-22 22:02:15 +0900

    Chapter 7

---
+++ Chapter 7/list.py
@@ -0,0 +1,52 @@
+# empty_list = []
+# weekdays = ['Monday', 'Tuesday', 'Wednesdy', 'Thursday', 'Friday']
+# big_birds = ['emu', 'ostrich', 'cassowary']
+# first_names = ['Graham', 'John', 'Terry', 'Terry', 'Michael']
+# leap_years = [2000, 2004, 2008]
+# randomneww = ['Punxsatawney', {'groundhog': 'Phil'}, 'Feb. 2']
+
+# another_empty_list = list()
+# print(another_empty_list)
+
+# print(list('cat'))
+
+# a_tuple = ('ready', 'fire', 'aim')
+# print(list(a_tuple))
+
+# talk_like_a_pirate_day = '9/19/2019'
+# print(talk_like_a_pirate_day.split('/'))
+
+# splitme = 'a/b//c/d///e'
+# print(splitme.split('/'))
+
+# splitme = 'a/b//c/d///e'
+# print(splitme.split('//'))
+
+# marxes = ['Groucho', 'Chico', 'Harpo']
+# print(marxes[0])
+# print(marxes[1])
+# print(marxes[2])
+
+# print(marxes[-1])
+# print(marxes[-2])
+# print(marxes[-3])
+
+# marxes = ['Groucho', 'Chico', 'Harpo']
+# print(marxes[5])
+# print(marxes[-5])
+
+# marxes = ['Groucho', 'Chico', 'Harpo']
+# print(marxes[0:2])
+# print(marxes[::2])
+# print(marxes[::-2])
+# print(marxes[::-1])
+
+# marxes = ['Groucho', 'Chico', 'Harpo']
+# marxes.reverse()
+# print(marxes)
+
+# marxes = ['Groucho', 'Chico', 'Harpo']
+# print(marxes[4:])
+# print(marxes[-6:])
+# print(marxes[-6:-2])
+# print(marxes[-6:-4])
