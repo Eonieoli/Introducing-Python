a95f0158ff1ff1f295734c25dd0cf25de12271aa
Jaehyeok Kim <rublin322@gmail.com>
2026-02-17 15:46:23 +0900

    Chapter 9

---
+++ Chapter 9/recursive.py
@@ -0,0 +1,17 @@
+# def dive():
+#     return dive()
+
+# dive()
+
+
+def flatten(lol):
+    for item in lol:
+        if isinstance(item, list):
+            for subitem in flatten(item):
+                yield subitem
+        else:
+            yield item
+
+lol = [1, 2, [3, 4, 5], [6, [7, 8, 9], []]]
+print(flatten(lol))
+print(list(flatten(lol)))
