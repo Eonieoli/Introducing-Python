d25cd5560b9b48dafb3e9f5f943de1047c4a5a27
Jaehyeok Kim <rublin322@gmail.com>
2026-03-04 13:33:14 +0900

    Chapter 11

---
+++ Chapter 11/problem.py
@@ -0,0 +1,42 @@
+# 1
+# import zoo
+
+# print(zoo.hours())
+
+
+# 2
+# import zoo as menagerie
+
+# print(menagerie.hours())
+
+
+# 3
+# from zoo import hours
+
+# print(hours())
+
+
+# 4
+# from zoo import hours as info
+
+# print(info())
+
+
+# 5
+plain = {'a': 1, 'b': 2, 'c': 3}
+print(plain)
+
+
+# 6
+from collections import OrderedDict
+
+fancy = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+print(fancy)
+
+
+# 7
+from collections import defaultdict
+
+dict_of_lists = defaultdict(str)
+dict_of_lists['a'] = 'something for a'
+print(dict_of_lists['a'])
