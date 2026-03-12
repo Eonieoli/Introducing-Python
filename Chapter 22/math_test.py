e5953fc6b529e33ad1773960d7101129223151b2
Jaehyeok Kim <rublin322@gmail.com>
2026-03-12 22:47:21 +0900

    Chapter 22

---
+++ Chapter 22/math_test.py
@@ -0,0 +1,42 @@
+import math
+
+print(math.pi)
+print(math.e)
+
+print(math.fabs(98.6))
+print(math.fabs(-271.1))
+
+print(math.floor(98.6))
+print(math.floor(-271.1))
+print(math.ceil(98.6))
+print(math.ceil(-271.1))
+
+print(math.factorial(0))
+print(math.factorial(1))
+print(math.factorial(2))
+print(math.factorial(3))
+print(math.factorial(10))
+
+print(math.log(1.0))
+print(math.log(math.e))
+
+print(math.log(8, 2))
+
+print(math.pow(2, 3))
+
+print(2 ** 3)
+print(2.0 ** 3)
+
+print(math.sqrt(100.0))
+
+# print(math.sqrt(-100.0))
+
+x = 3.0
+y = 4.0
+print(math.hypot(x, y))
+
+print(math.sqrt(x*x + y*y))
+print(math.sqrt(x**2 + y**2))
+
+print(math.radians(180.0))
+print(math.degrees(math.pi))
